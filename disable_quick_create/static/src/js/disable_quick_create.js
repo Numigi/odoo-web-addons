2d494dc11b5028a9b3079acc2e57af790b11ca69
David Dufresne <dufresne.david.2@gmail.com>
2018-03-28 13:00:34 -0400

    Fix codacy issues

--- disable_quick_create/static/src/js/disable_quick_create.js
+++ disable_quick_create/static/src/js/disable_quick_create.js
@@ -3,11 +3,11 @@
     © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
     License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
 */
-odoo.define('disable_quick_create', function(require) {
+odoo.define("disable_quick_create", function(require) {
     "use strict";
 
-    var relational_fields = require('web.relational_fields');
-    var rpc = require('web.rpc');
+    var relationalFields = require("web.relational_fields");
+    var rpc = require("web.rpc");
 
     var models = [];
 
@@ -15,8 +15,8 @@ odoo.define('disable_quick_create', function(require) {
         model: "ir.model",
         method: "search_read",
         args:[
-            [['disable_create_edit','=', true]],
-            ['model'],
+            [["disable_create_edit", "=", true]],
+            ["model"],
         ],
     }).then(function(result) {
         result.forEach(function(el){
@@ -24,7 +24,7 @@ odoo.define('disable_quick_create', function(require) {
         })
     });
 
-    relational_fields.FieldMany2One.include({
+    relationalFields.FieldMany2One.include({
         init: function() {
             this._super.apply(this, arguments);
 
