2f31147f0e64d08fc7254519acf79d8117404561
David Dufresne <dufresne.david.2@gmail.com>
2018-04-05 17:13:31 -0400

    Add ui_color_red

---
+++ ui_color_red/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Savoir-faire Linux
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "UI Color Red",
+    'version': '1.0.0',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    "category": "UI",
+    "summary": "Red web interface",
+    "depends": ['web_enterprise'],
+    "data": [
+        'views/ui_color.xml',
+    ],
+    "installable": True,
+}
