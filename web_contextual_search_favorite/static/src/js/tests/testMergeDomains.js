26b17b65c9bd238b94c79cc922a7d5e37140d4bf
David Dufresne <dufresne.david.2@gmail.com>
2018-06-21 00:04:33 -0400

    Codacy fix

--- web_contextual_search_favorite/static/src/js/tests/testMergeDomains.js
+++ web_contextual_search_favorite/static/src/js/tests/testMergeDomains.js
@@ -8,20 +8,20 @@ var domain3 = "['|', ('c', '=', 3), ('d', '=', 4)]";
 
 test("Merge 2 Domains Using And Operators", t => {
     var result = mergeDomainsWithAndOperators([domain1, domain2]);
-    t.true(result === "[('a', '=', 1), ('b', '=', 1), ('b', '=', 2)]")
+    t.true(result === "[('a', '=', 1), ('b', '=', 1), ('b', '=', 2)]");
 });
 
 test("Merge 3 Domains Using And Operators", t => {
     var result = mergeDomainsWithAndOperators([domain1, domain2, domain3]);
-    t.true(result === "[('a', '=', 1), ('b', '=', 1), ('b', '=', 2), \"|\", ('c', '=', 3), ('d', '=', 4)]")
+    t.true(result === "[('a', '=', 1), ('b', '=', 1), ('b', '=', 2), \"|\", ('c', '=', 3), ('d', '=', 4)]");
 });
 
 test("Merge 2 Domains Using Or Operators", t => {
     var result = mergeDomainsWithOrOperators([domain1, domain2]);
-    t.true(result === "[\"|\", ('a', '=', 1), \"&\", ('b', '=', 1), ('b', '=', 2)]")
+    t.true(result === "[\"|\", ('a', '=', 1), \"&\", ('b', '=', 1), ('b', '=', 2)]");
 });
 
 test("Merge 3 Domains Using Or Operators", t => {
     var result = mergeDomainsWithOrOperators([domain1, domain2, domain3]);
-    t.true(result === "[\"|\", \"|\", ('a', '=', 1), \"&\", ('b', '=', 1), ('b', '=', 2), \"|\", ('c', '=', 3), ('d', '=', 4)]")
+    t.true(result === "[\"|\", \"|\", ('a', '=', 1), \"&\", ('b', '=', 1), ('b', '=', 2), \"|\", ('c', '=', 3), ('d', '=', 4)]");
 });
