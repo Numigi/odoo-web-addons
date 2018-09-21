2eab13cea9a13f3645d2a048a5203661d0b70323
David Dufresne <david.dufresne@numigi.net>
2018-09-21 10:46:29 -0400

    web_custom_label: fix method fields_get

--- web_custom_label/models.py
+++ web_custom_label/models.py
@@ -95,13 +95,14 @@ class BaseWitCustomLabels(models.AbstractModel):
 
     _inherit = 'base'
 
-    def fields_get(self, *args, **kwargs):
+    @api.model
+    def fields_get(self, allfields=None, attributes=None):
         """Add the custom labels to the fields metadata.
 
         The method is used to query the fields metadata.
         This data is used by search filters / group by to display the field names.
         """
-        fields = super().fields_get(*args, **kwargs)
+        fields = super().fields_get(allfields, attributes)
         lang = self.env.context.get('lang') or self.env.user.lang
         labels = self.env['web.custom.label']._find_labels(self._name, lang)
         set_custom_labels_on_fields(labels, fields)
