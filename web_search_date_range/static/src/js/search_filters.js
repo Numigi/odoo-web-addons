21351092ed2baa6c2b68ba86324babe0c0d6d96e
David Dufresne <david.dufresne@numigi.net>
2018-10-18 17:18:22 -0400

    Port web addons to odoo 12.0

--- web_search_date_range/static/src/js/search_filters.js
+++ web_search_date_range/static/src/js/search_filters.js
@@ -7,9 +7,8 @@ odoo.define("web_search_date_range.filter_menu_with_date_range", function(requir
 
 var Class = require("web.Class");
 var Widget = require("web.Widget");
-var FilterMenu = require("web.FilterMenu");
+var FilterMenu = require("web.FiltersMenu");
 var searchInputs = require("web.search_inputs");
-var pyeval = require("web.pyeval");
 var ajax = require("web.ajax");
 
 
@@ -81,14 +80,11 @@ var SearchDateRangeProposition = Widget.extend({
 
 
 FilterMenu.include({
-    events: _.extend({}, FilterMenu.prototype.events, {
-        "click .o_add_date_range": "append_date_range_proposition",
-    }),
     start(){
         this._super.apply(this, arguments);
         var self = this;
 
-        var model = this.searchview.dataset.model;
+        var model = this.getParent().dataset.model;
         filterRegistry.getFilters(model).then(function(filterArray){
             // Group the filters by field
             var filterArraysByField = _.values(_.groupBy(filterArray, function(f){return f.field;}));
@@ -114,21 +110,29 @@ FilterMenu.include({
 
         var self = this;
         var proposition = new SearchDateRangeProposition(this, sortedfilterArray[0].field_label);
-        proposition.insertBefore(this.$add_filter);
+        proposition.insertBefore(this.$addCustomFilter);
 
         // Create the search items
         var filterWidgets = sortedfilterArray.map(function(filter){
             return self._createDateRangeFilterWidget(filter);
         });
-        var filterGroup = new searchInputs.FilterGroup(filterWidgets, this.searchview);
-        filterGroup.insertBefore(this.$add_filter);
-        $("<li class=\"divider\">").insertBefore(this.$add_filter);
+        var filterGroup = new searchInputs.FilterGroup(filterWidgets, this.getParent(), {}, {});
+        filterGroup.insertBefore(this.$addCustomFilter);
+        $("<li class=\"divider\">").insertBefore(this.$addCustomFilter);
 
         // Show / hide filters when clicking on the field name
         proposition.$el.click(function(){
-            proposition.$el.toggleClass("o_closed_menu");
-            proposition.$el.toggleClass("o_open_menu");
-            filterGroup.$el.toggle();
+            var menu = proposition.$el;
+            menu.toggleClass("o_closed_menu");
+            menu.toggleClass("o_open_menu");
+
+            var subMenu = filterGroup.$el;
+            if(subMenu.is(":visible")){
+                subMenu.hide();
+            }
+            else {
+                subMenu.show()
+            }
         });
 
         // Hide filters by default
@@ -150,7 +154,7 @@ FilterMenu.include({
             children: [],
             tag: "filter",
         };
-        return new searchInputs.Filter(filterXMLNode, this.searchview);
+        return new searchInputs.Filter(filterXMLNode, this.getParent());
     },
 });
 
