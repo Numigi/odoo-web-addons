c712c233516c76643446504b0bb95f1c404916cf
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-03-12 12:53:25 -0400

    Add module web_handle_condition

---
+++ web_handle_condition/static/src/js/handle.js
@@ -0,0 +1,43 @@
+/*
+    © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+    License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
+*/
+odoo.define("web_handle_condition", function(require) {
+"use strict";
+
+const basicFields = require("web.basic_fields");
+const handleInvisibleField = "handle_widget_invisible";
+
+basicFields.HandleWidget.include({
+    _render() {
+        this._super.apply(this, arguments);
+
+        if(this._isHandleInvisible()){
+            this._hideHandle();
+        }
+    },
+
+    _reset() {
+        this._super.apply(this, arguments)
+
+        if(this._isHandleInvisible()){
+            this._hideHandle();
+        }
+        else {
+            this._showHandle();
+        }
+    },
+
+    _isHandleInvisible() {
+        return Boolean(this.record.data[handleInvisibleField]);
+    },
+
+    _hideHandle() {
+        this.$el.addClass("d-none");
+    },
+
+    _showHandle() {
+        this.$el.removeClass("d-none");
+    },
+});
+});
