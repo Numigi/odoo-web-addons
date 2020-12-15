d87858b3a324fd34ff3772b3f1a4663310b64b9e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-15 12:22:27 -0500

    TA#28135 add module web_editor_backend_context

---
+++ web_editor_backend_context/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2020 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Web Editor Backend Context",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Web",
+    "summary": "Fix the user context for web editor assets",
+    "depends": ["web_editor"],
+    "data": ["views/assets.xml",],
+    "installable": True,
+    "auto_install": True,
+}
