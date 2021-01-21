9ba83f523d188521069bbc4e744c438a09a42083
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-01-21 15:36:32 -0500

    TA#30546 fix domain propagation to dashboards (#57)

--- web_contextual_search_favorite/__manifest__.py
+++ web_contextual_search_favorite/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Web Contextual Search Favorite',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
