af24d377ad7d606aad4105b81153345300bf40dd
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-12 17:09:42 -0500

    Port google_attachment to v14

--- google_attachment/__manifest__.py
+++ google_attachment/__manifest__.py
@@ -2,19 +2,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Google Drive Attachment',
-    'version': '1.0.0',
-    'category': 'Document Management',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://bit.ly/numigi-com',
-    'license': 'LGPL-3',
-    'depends': [
-        'base_setup', 'document',
+    "name": "Google Drive Attachment",
+    "version": "1.0.0",
+    "category": "Document Management",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "depends": ["base_setup", "mail"],
+    "data": ["views/assets.xml", "views/res_config_settings.xml",],
+    "qweb": [
+        "static/src/components/google_attachment.xml",
+        "static/src/components/attachment_box.xml",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/res_config_settings.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
