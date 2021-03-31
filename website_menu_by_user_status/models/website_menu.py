d40a447da2d8681cb9d3b6b0142b1f0485102a1b
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-31 10:07:29 -0400

    Port website_menu_by_user_status to v14

--- website_menu_by_user_status/models/website_menu.py
+++ website_menu_by_user_status/models/website_menu.py
@@ -6,9 +6,6 @@ from odoo import api, fields, models
 
 
 class WebsiteMenu(models.Model):
-    """Improve website.menu with adding booleans that drive
-    if the menu is displayed when the user is logger or not.
-    """
 
     _inherit = 'website.menu'
 
@@ -25,37 +22,19 @@ class WebsiteMenu(models.Model):
              "when the user is not logged."
     )
 
-    @api.one
-    def _compute_visible(self):
-        """Display the menu item whether the user is logged or not."""
-        super()._compute_visible()
-        if not self.is_visible:
-            return
-
-        if self.env.user == self.env.ref('base.public_user'):
-            self.is_visible = self.user_not_logged
-        else:
-            self.is_visible = self.user_logged
-
-
-class WebsiteMenuVisibleForSpecificGroups(models.Model):
-    """Optionally display the menu item for a given list of user groups."""
-
-    _inherit = 'website.menu'
-
     group_ids = fields.Many2many(
         'res.groups', 'website_menu_group_rel', 'menu_id', 'group_id', 'Group',
         help="If filled, the menu item is only visible to the users "
         "from one of the selected groups."
     )
 
-    @api.one
     def _compute_visible(self):
-        """Display the menu item whether the user is logged or not."""
         super()._compute_visible()
-        if not self.is_visible:
-            return
 
-        if self.group_ids:
-            user_has_any_group = bool(self.env.user.groups_id & self.group_ids)
-            self.is_visible = user_has_any_group
+        public_user = self.env.ref('base.public_user')
+
+        for menu in self:
+            if menu.env.user == public_user:
+                menu.is_visible = menu.is_visible and menu.user_not_logged
+            else:
+                menu.is_visible = menu.is_visible and menu.user_logged
