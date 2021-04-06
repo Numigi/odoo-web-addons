822ee8d5aee145bc878b20d33d954df01adbca3e
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-04-06 09:10:18 -0400

    TA#35155 port web_search_date_range to v14 (#63)
    * Port web_search_date_range to v14

---
+++ web_search_date_range/static/src/js/search_filters.js
@@ -0,0 +1,68 @@
+/*
+    © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+    License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
+*/
+odoo.define("web_search_date_range/static/src/js/search_filters.js", function(require) {
+    "use strict";
+
+var Class = require("web.Class");
+var ajax = require("web.ajax");
+
+
+/**
+ * A class responsible for managing the metadata related to date filters.
+ */
+var DateRangeFilterRegistry = Class.extend({
+    init(){
+        this._deferred = new $.Deferred();
+        this._filtersFetched = false;
+        this._filtersByModel = new Map();
+    },
+    /**
+     * Get an array of filter values for the given model.
+     *
+     * @param {String} model: the model name
+     */
+    getFilters(model){
+        var self = this;
+
+        if(!this._filtersFetched){
+            this._fetchFilters().then(function(){
+                self._deferred.resolve();
+            });
+            this._filtersFetched = true;
+        }
+
+        return $.when(this._deferred).then(function(){
+            return self._filtersByModel.get(model) || [];
+        });
+    },
+    /**
+     * Fetch the filters from the server.
+     *
+     * All filters are cached on the first query.
+     * This method is called only one time per session.
+     */
+    _fetchFilters(){
+        var self = this;
+        return ajax.rpc("/web/dataset/call_kw/search.date.range.filter/get_filter_list", {
+            model: "search.date.range.filter",
+            method: "get_filter_list",
+            args: [],
+            kwargs: {},
+        }).then(function(result){
+            result.forEach(function(filter){
+                if(!self._filtersByModel.has(filter.model)){
+                    self._filtersByModel.set(filter.model, []);
+                }
+                self._filtersByModel.get(filter.model).push(filter);
+            });
+        });
+    },
+});
+
+var filterRegistry = new DateRangeFilterRegistry();
+
+return filterRegistry
+
+});
---
+++ web_search_date_range/tests/test_search_date_range.py
@@ -0,0 +1,159 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from ddt import data, ddt
+from dateutil.relativedelta import relativedelta
+from freezegun import freeze_time
+from datetime import datetime
+from odoo.tests import common
+
+
+@ddt
+class TestSearchDateRange(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.model = cls.env.ref('base.model_res_partner')
+        cls.field = cls.env.ref('base.field_res_partner__create_date')
+
+    def _eval_filter_domain(self, range_ref):
+        date_range = self.env.ref('web_search_date_range.{range_ref}'.format(range_ref=range_ref))
+        return date_range.generate_domain(self.field.name)
+
+    def test_eval_domain_for_range_before_today(self):
+        with freeze_time('2018-05-18'):
+            domain = self._eval_filter_domain('range_before_today')
+            self.assertEqual(domain, [
+                ('create_date', '<', '2018-05-18'),
+            ])
+
+    def test_eval_domain_for_range_today(self):
+        with freeze_time('2018-05-18'):
+            domain = self._eval_filter_domain('range_today')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-18'),
+                ('create_date', '<', '2018-05-19'),
+            ])
+
+    def test_eval_domain_for_next_fifteen_days(self):
+        with freeze_time('2018-05-18'):
+            domain = self._eval_filter_domain('range_next_fifteen_days')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-18'),
+                ('create_date', '<', '2018-06-02'),
+            ])
+
+    @data('2018-05-14', '2018-05-18', '2018-05-20')
+    def test_eval_domain_for_range_current_week(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_current_week')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-14'),
+                ('create_date', '<', '2018-05-21'),
+            ])
+
+    @data('2018-05-14', '2018-05-18', '2018-05-20')
+    def test_eval_domain_for_range_next_week(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_next_week')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-21'),
+                ('create_date', '<', '2018-05-28'),
+            ])
+
+    @data('2018-05-14', '2018-05-18', '2018-05-20')
+    def test_eval_domain_for_range_previous_week(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_previous_week')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-07'),
+                ('create_date', '<', '2018-05-14'),
+            ])
+
+    @data('2018-05-01', '2018-05-18', '2018-05-31')
+    def test_eval_domain_for_range_current_month(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_current_month')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-05-01'),
+                ('create_date', '<', '2018-06-01'),
+            ])
+
+    @data('2018-05-01', '2018-05-18', '2018-05-31')
+    def test_eval_domain_for_range_next_month(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_next_month')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-06-01'),
+                ('create_date', '<', '2018-07-01'),
+            ])
+
+    @data('2018-05-01', '2018-05-18', '2018-05-31')
+    def test_eval_domain_for_range_previous_month(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_previous_month')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-04-01'),
+                ('create_date', '<', '2018-05-01'),
+            ])
+
+    @data('2018-01-01', '2018-05-18', '2018-12-31')
+    def test_eval_domain_for_range_current_year(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_current_year')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2018-01-01'),
+                ('create_date', '<', '2019-01-01'),
+            ])
+
+    @data('2018-01-01', '2018-05-18', '2018-12-31')
+    def test_eval_domain_for_range_next_year(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_next_year')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2019-01-01'),
+                ('create_date', '<', '2020-01-01'),
+            ])
+
+    @data('2018-01-01', '2018-05-18', '2018-12-31')
+    def test_eval_domain_for_range_previous_year(self, today):
+        with freeze_time(today):
+            domain = self._eval_filter_domain('range_previous_year')
+            self.assertEqual(domain, [
+                '&',
+                ('create_date', '>=', '2017-01-01'),
+                ('create_date', '<', '2018-01-01'),
+            ])
+
+
+class TestPartner(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env["res.partner"].search([]).write({'date': False})
+
+        cls.date_1 = datetime.now().date()
+        cls.date_2 = cls.date_1 - relativedelta(days=30)
+
+        cls.partner_1 = cls.env["res.partner"].create({"name": "Partner 1", "date": cls.date_1})
+        cls.partner_2 = cls.env["res.partner"].create({"name": "Partner 1", "date": cls.date_2})
+
+    def test_search_partner(self):
+        range_today = self.env.ref('web_search_date_range.range_today')
+
+        with freeze_time(self.date_1):
+            partners = self.env["res.partner"].search([('date', 'range', range_today.id)])
+
+        assert partners == self.partner_1
