e98750f9a3cf54e2b3842689e46790d89ae7e8f5
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-11-04 14:25:54 -0400

    TA#40377 add website_landing_template

---
+++ website_landing_template/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Website Landing Page",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Web",
+    "summary": "Add a template website page for creating landing pages",
+    "depends": ["website"],
+    "data": [
+        "views/layout.xml",
+        "data/website_page.xml",
+    ],
+    "installable": True,
+}
