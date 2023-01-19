f1d12bee412cd37c1f48d2e96ceda5a4d9b06f78
Houda BEN TALEB <houda.bentaleb@gmail.com>
2023-01-19 22:20:02 +0100

    TA#47984 [MIG]:Rm website_blog_internal from docker file

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -22,7 +22,7 @@
         'web_list_column_width',
         'web_trash_condition',
         # 'website_google_analytics_fixed',
-        'website_blog_internal',
+        #'website_blog_internal',
         'website_landing_template',
         'website_menu_by_user_status',
     ],
