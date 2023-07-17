c77d6d42b8e7d7ee2f8cd70e8f8630d68f10ff34
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-07-17 21:01:33 +0300

    TA#55135_14 [ADD] web_hide_db_manager_link : add new module

---
+++ web_hide_db_manager_link/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright Anubía, soluciones en la nube,SL (http://www.anubia.es)
+# Alejandro Santana <alejandrosantana@anubia.es>
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hide link to database manager in login screen',
+    'version': "14.0.1.0.0",
+    'category': 'Web',
+    'license': 'AGPL-3',
+    'author': 'Alejandro Santana, Odoo Community Association (OCA)',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'summary': 'Hide link to database manager in login screen',
+    'depends': ['web'],
+    'data': ['views/webclient_templates.xml'],
+    'installable': True,
+}
