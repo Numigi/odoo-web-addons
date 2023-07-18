5bb5249eacfb7d2e3d128a9f0be7e14c8537604f
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-07-18 18:49:03 +0300

    TA#55135 [ADD] web_hide_db_manager_link : new module (#85)
    * TA#55135 [ADD] web_hide_db_manager_link : new module
    
    * [DEL] unused svg file

---
+++ web_hide_db_manager_link/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright Anubía, soluciones en la nube,SL (http://www.anubia.es)
+# Alejandro Santana <alejandrosantana@anubia.es>
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
