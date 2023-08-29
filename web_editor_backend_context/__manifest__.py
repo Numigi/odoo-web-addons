1df7ad11e75fc306fd20e689d0ccadb6bf72cbd0
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-08-29 20:09:15 +0300

    [UPD] module version

--- web_editor_backend_context/__manifest__.py
+++ web_editor_backend_context/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Web Editor Backend Context",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
