98ebf234a33de37bba0ae2d14face5aa114d7823
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2023-10-11 18:31:39 +0100

    TA#57106 [ADD] website_geoip

---
+++ website_geoip/README.rst
@@ -0,0 +1,12 @@
+Website GeoIP
+=============
+This module adds the visitor website GeoIP datas as a session variable
+
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+
+More information
+----------------
+* Meet us at https://bit.ly/numigi-com
---
+++ website_geoip/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Website GeoIP',
+    'version': '1.0.1',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Website',
+    'depends': [
+        'website',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
