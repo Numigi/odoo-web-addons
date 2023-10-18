18ccecb8535710ee45823939c7374c1862c50d34
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-10-19 00:05:56 +0300

    TA#56191 [ADD] resize_observer_error_catcher : handling resize observer error (#572)
    * TA#56191 [ADD] resize_observer_error_catcher : handling resize observer error
    
    ---------
    
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

---
+++ resize_observer_error_catcher/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Resize Observer Error Catcher",
+    "summary": "Skip Resize Observer interface error when zooming.",
+    "version": "1.0.1",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ['views/assets.xml',],
+    "installable": True,
+}
