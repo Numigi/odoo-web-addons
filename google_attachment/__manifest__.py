ab880c9c5cb3c32c2093230c185b830cba33a5de
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-10-27 00:54:27 +0300

    TA#58336 [FIX] google_attachment : load newly added

--- google_attachment/__manifest__.py
+++ google_attachment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Google Drive Attachment",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "category": "Document Management",
     "author": "Numigi",
     "maintainer": "Numigi",
