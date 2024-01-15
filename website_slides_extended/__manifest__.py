93ef0227ea44a0b8a7c39ca633a45a04b29ce00a
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-01-15 12:23:56 -0500

    [FIX] Correct module name

--- website_slides_extended/__manifest__.py
+++ website_slides_extended/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Project Projected Hours",
+    "name": "Website Slides Extended",
     "version": "1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
