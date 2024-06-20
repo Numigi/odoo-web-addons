01bea0e232c564dff2e23a87de471ffd88182c51
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-06-20 10:17:06 -0400

    TA#66731 [ADD][16.0] Initiate branch (#115)
    * TA#66731 [ADD][16.0] Initiate branch

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,6 +11,7 @@
     "category": "Other",
     "summary": "Install all addons required for testing.",
     "depends": [
+        "web",
     ],
     "installable": True,
 }
--- web_contextual_search_favorite/static/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {  
-  entry: ["./src/js/main.js"],
-  mode: "production",
-  devtool: false,
-  output: {filename: "webContextualSearchFavorite.js"}
-};
