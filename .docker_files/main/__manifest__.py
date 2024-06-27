69d3a1a77f516bebe48e9904dd084029c67c0302
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-06-26 21:49:53 -0400

    Update __manifest__.py

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -13,7 +13,7 @@
     "depends": [
         "disable_quick_create",
         "google_attachment",
-        # "multi_website_user_access", TA#63329 - Commented out to avoid conflict
+        "multi_website_user_access",
         "resize_observer_error_catcher",
         "web_contextual_search_favorite",
         "web_custom_label",
