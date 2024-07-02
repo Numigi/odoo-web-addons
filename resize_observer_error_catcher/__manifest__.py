f285f69e5efde14e75cb603b6c42a0ec61b48860
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-07-02 22:32:03 +0300

    TA#66892 [MIG][16.0] resize_observer_error_catcher (#117)
    * TA#66892 [MIG][16.0] resize_observer_error_catcher

---
+++ resize_observer_error_catcher/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Resize Observer Error Catcher",
+    "summary": "Skip Resize Observer interface error when zooming.",
+    "version": "16.0.1.0.0",
+    "website": "https://bit.ly/numigi-com",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "resize_observer_error_catcher/static/src/js/resize_observer_catcher.js",
+        ],
+    },
+    "installable": True,
+}
