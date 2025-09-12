9103ebd2eb840a79a64f0859008c1577b32485f2
Abdellatif Benzbiria <odoo-base@numigi.com>
2025-09-12 13:51:55 +0100

    [Fix] xml product_json_id

--- website_sale_jsonld_schema/tests/test_product_jsonld.py
+++ website_sale_jsonld_schema/tests/test_product_jsonld.py
@@ -2,7 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
+from unittest.mock import Mock
 import json
+
 
 
 class TestProductJsonLd(TransactionCase):
@@ -17,6 +19,11 @@ class TestProductJsonLd(TransactionCase):
             {'name': "Test Product", 'list_price': 99.99, 'default_code': "SKU123",
                 'weight': 1.5,
                 'public_categ_ids': [(6, 0, [self.public_category.id])], })
+
+        self.website = self.env['website'].create({'name': 'Test Website',
+            'company_id': self.env.ref('base.main_company').id, })
+        self.website.get_current_website = Mock(return_value=self.website)
+        self.website.currency_id = self.env.ref('base.USD')
 
     def test_jsonld_escape(self):
         """ Vérifie que les caractères spéciaux sont correctement échappés """
@@ -47,15 +54,12 @@ class TestProductJsonLd(TransactionCase):
 
     def test_render_jsonld_template(self):
         """ Vérifie que le template JSON-LD contient les champs attendus """
-        website = self.env['website'].get_current_website()
         rendered = self.env['ir.qweb']._render(
             'website_sale_jsonld_schema.product_json_ld_schema',
-            values={'product': self.product, 'website': website},
-        )
-        # Convertir en dict Python pour vérifier les champs
+            values={'product': self.product, 'website': self.website}, )
         data_str = rendered.decode() if isinstance(rendered, bytes) else rendered
-        json_str = data_str.split("<script type=\"application/ld+json\">")[1].split(
-            "</script>")[0]
+        json_str = \
+        data_str.split("<script type=\"application/ld+json\">")[1].split("</script>")[0]
         data = json.loads(json_str)
 
         self.assertEqual(data["@type"], "Product")
