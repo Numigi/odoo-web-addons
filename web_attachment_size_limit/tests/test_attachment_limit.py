af0d00c6cef277b50a1646c90566de3ce2dbf09b
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-03-30 18:54:41 +0000

    TA#82839 [14.0][FIX] web_attachment_size_limit TU

--- web_attachment_size_limit/tests/test_attachment_limit.py
+++ web_attachment_size_limit/tests/test_attachment_limit.py
@@ -1,34 +1,47 @@
 # © Numigi (tm) and all its contributors (https://numigi.com/r/home)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import io
-import json
+from unittest.mock import MagicMock, patch
 
-from odoo.tests.common import HttpCase, tagged
+from odoo.tests.common import TransactionCase, tagged
+from odoo.addons.web_attachment_size_limit.controllers.main import BinaryUploadLimit
 
 
 @tagged("-at_install", "post_install")
-class TestAttachmentSizeLimit(HttpCase):
+class TestAttachmentSizeLimit(TransactionCase):
 
     def setUp(self):
         super().setUp()
         self.env["ir.config_parameter"].sudo().set_param(
-            "web_attachment_size_limit.max_upload_size", "100"
+            "web.max_file_upload_size", "100"
         )
+        self.controller = BinaryUploadLimit()
 
-    def _upload_file(self, content: bytes, filename="test.txt"):
-        files = {"ufile": (filename, io.BytesIO(content), "text/plain")}
-        data = {"model": "res.users", "id": str(self.env.user.id)}
-        return self.url_open("/web/binary/upload_attachment", data=data, files=files)
+    def _test_upload(self, content_length: int):
+        ufile_mock = MagicMock()
+        ufile_mock.tell.return_value = content_length
+
+        patch_req = patch("odoo.addons.web_attachment_size_limit.controllers.main.request")
+        patch_sup = patch("odoo.addons.web.controllers.main.Binary.upload_attachment")
+
+        with patch_req as mock_req, patch_sup as mock_sup:
+            mock_req.env = self.env
+            mock_req.httprequest.files.getlist.return_value = [ufile_mock]
+            mock_sup.return_value = '{"id": 123}'
+
+            res = self.controller.upload_attachment(
+                model="res.users",
+                id=self.env.user.id,
+                ufile=ufile_mock,
+            )
+            return res, mock_sup.called
 
     def test_02_upload_too_large(self):
-        response = self._upload_file(b"x" * 200, filename="too_big.txt")
-        assert response.status_code == 200
-        assert "File too large" in response.text
+        response, super_called = self._test_upload(200)
+        assert "File too large" in response
+        assert not super_called
 
     def test_03_upload_success(self):
-        response = self._upload_file(b"x" * 50, filename="small_file.txt")
-        assert response.status_code == 200
-        payload = json.loads(response.text)
-        assert "id" in payload
-        assert self.env["ir.attachment"].browse(payload["id"]).exists()
+        response, super_called = self._test_upload(50)
+        assert response == '{"id": 123}'
+        assert super_called
