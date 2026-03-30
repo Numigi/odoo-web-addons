7b3636b699ed0f35f5207ef32f0df9684f87d78e
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-03-30 18:44:39 +0000

    TA#82839 [14.0][FIX] web_attachment_size_limit TU

--- web_attachment_size_limit/tests/test_attachment_limit.py
+++ web_attachment_size_limit/tests/test_attachment_limit.py
@@ -4,22 +4,17 @@
 import io
 import json
 
-from odoo import SUPERUSER_ID, api
 from odoo.tests.common import HttpCase, tagged
 
 
 @tagged("-at_install", "post_install")
 class TestAttachmentSizeLimit(HttpCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        # HttpCase lacks cls.env, so we manage the environment manually
-        with api.Environment.manage():
-            env = api.Environment(cls.cr, SUPERUSER_ID, {})
-            env["ir.config_parameter"].sudo().set_param(
-                "web_attachment_size_limit.max_upload_size", "100"
-            )
+    def setUp(self):
+        super().setUp()
+        self.env["ir.config_parameter"].sudo().set_param(
+            "web_attachment_size_limit.max_upload_size", "100"
+        )
 
     def _upload_file(self, content: bytes, filename="test.txt"):
         files = {"ufile": (filename, io.BytesIO(content), "text/plain")}
