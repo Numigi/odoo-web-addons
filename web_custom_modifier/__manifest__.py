6495b96c2b466a862191595fdb64fa7426f1cd2d
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-04-15 13:57:26 +0000

    TA#83812 [18.0][ADD] web_custom_modifier

--- web_custom_modifier/__manifest__.py
+++ web_custom_modifier/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Web Custom Modifier",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
