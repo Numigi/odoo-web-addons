20c4d5233eb1b3bbd4f4ef0f9332f47d132555ab
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-11 16:29:30 +0000

    [18.0][ADD] web_db_name_display

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,9 +11,9 @@
     "category": "Other",
     "summary": "Install all addons required for testing.",
     "depends": [
-                "web_custom_label",
-                "web_custom_modifier",
-                "web_db_name_display"
-                ],
+        "web_custom_label",
+        "web_custom_modifier",
+        "web_db_name_display",
+    ],
     "installable": True,
 }
