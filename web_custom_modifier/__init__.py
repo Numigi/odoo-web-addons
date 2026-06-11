56e908bb8454f9634f1685ef6ba3e0127c447679
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-11 16:12:45 +0000

    [18.0][ADD] web_db_name_display

--- web_custom_modifier/__init__.py
+++ web_custom_modifier/__init__.py
@@ -1,4 +1,6 @@
 # Copyright 2023-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import models
+"""Initialize the module module."""
+
+from . import models  # noqa: F401
