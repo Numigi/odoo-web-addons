2fd5357d7e4607290856921652df7cfca974f81b
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-06-24 16:09:55 +0000

    [18.0][IMP] web_db_display name

--- web_db_name_display/models/__init__.py
+++ web_db_name_display/models/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2026-today Numigi and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import res_users # noqa: F401
-from . import ir_http # noqa: F401
+from . import res_users  # noqa: F401
+from . import ir_http  # noqa: F401
